82415a4c4408310273a066ef35814c38fdf9d174
vtferrari <vtferrari@hotmail.com>
2016-09-18 23:42:25 -0300

    começando a inicializar as coisas com o linux

--- meu_init.sh
+++ meu_init.sh
@@ -7,13 +7,14 @@ if which apache2 > /dev/null ; then
 else
     sudo apt-get install apache2 -y
 fi
-
+sudo chown $USER /var/www/*
 rm -rf /var/www/html/*
 
 if [ ! -d "/home/$USER/FatecSCSCriptografia" ]; then
    git clone https://github.com/vtferrari/FatecSCSCriptografia.git
 fi
 
-mv /home/$USER/FatecSCSCriptografia/* /var/www/html/
+mv /home/$USER/FatecSCSCriptografia/public_html/* /var/www/html/
 rm -rf /home/$USER/FatecSCSCriptografia
 
+firefox localhost
